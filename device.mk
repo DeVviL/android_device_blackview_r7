314e3828675d20ce6d7f4bd82efa903ad8172392
DeVviL <deuvil.den@gmail.com>
2020-08-08 03:36:20 +0300

    Add camera app

--- device.mk
+++ device.mk
@@ -73,6 +73,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     YGPS
 
+# Camera
+PRODUCT_PACKAGES += \
+	Snap
+
 # Telephony
 SIM_COUNT := 2
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
