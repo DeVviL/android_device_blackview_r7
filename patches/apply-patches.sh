9c21abb85cbf9c3344734119dd909c3f4ebb76c4
DeVviL <deuvil.den@gmail.com>
2020-08-11 00:39:35 +0300

    Disable frameworks/av patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -7,11 +7,11 @@ cd ../..
 cd packages/apps/Settings
 patch -p1 < ../../../device/blackview/r7/patches/0005-add-author-info-in-device-info.patch
 cd ../../..
-cd frameworks/av
-patch -p1 < ../../device/blackview/r7/patches/0006-fix-access-wvm-to-ReadOptions.patch
-patch -p1 < ../../device/blackview/r7/patches/0007-Disable-usage-of-get_capture_position.patch
-patch -p1 < ../../device/blackview/r7/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders.patch
-cd ../..
+#cd frameworks/av
+#patch -p1 < ../../device/blackview/r7/patches/0006-fix-access-wvm-to-ReadOptions.patch
+#patch -p1 < ../../device/blackview/r7/patches/0007-Disable-usage-of-get_capture_position.patch
+#patch -p1 < ../../device/blackview/r7/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders.patch
+#cd ../..
 cd system/netd
 patch -p1 < ../../device/blackview/r7/patches/netd.patch
 cd ../..
